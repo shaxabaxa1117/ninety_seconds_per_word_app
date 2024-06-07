5f68d0d945634e2c330ada0ed878a4321c4bc746
unknown <shaxabaca17@gmail.com>
2024-06-07 11:56:19 +0500

    abdulaziz

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:ninenty_second_per_word_app/fronted/style/app_colors.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    return Scaffold(
+        backgroundColor: AppColors.backgroundColor,
+        //resizeToAvoidBottomInset: false,
+        // appBar: AppBar(
+        //   backgroundColor: AppColors.headerColor,
+        //   centerTitle: true,
+        //   actions: [
+        //     IconButton(
+        //       onPressed: () {
+        //         if ((user == null)) {
+        //           Navigator.push(
+        //             context,
+        //             MaterialPageRoute(builder: (context) => const LoginScreen()),
+        //           );
+        //         } else {
+        //           Navigator.push(
+        //             context,
+        //             MaterialPageRoute(
+        //                 builder: (context) => const AccountScreen()),
+        //           );
+        //         }
+        //       },
+        //       icon: Icon(
+        //         Icons.person,
+        //         color: (user == null) ? Colors.black : Colors.yellow,
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        body: Text('data')
+        //  (user == null) ? const LoginScreen() : const HomeNavBarItemBuilder(),
+        // bottomNavigationBar: (user == null) ? Text('data') : Text(''),
+        );
+  }
+}
