60d17387bb34be04a34c743707cb6844d67bdf8d
unknown <shaxabaca17@gmail.com>
2024-06-07 11:58:47 +0500

    abdulaziz

---
+++ lib/fronted/style/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const backgroundColor = Color.fromARGB(255, 255, 255, 255);
+  static const headerColor = Color.fromARGB(255, 255, 255, 255);
+  static const textColor = Colors.black;
+  static const dividerColor = Color.fromARGB(255, 131, 131, 131);
+  static const iconColor = Color.fromARGB(255, 219, 34, 35);
+  static const iconGuardColor = Color(0xFF7F2828);
+}
---
+++ lib/fronted/style/app_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:ninenty_second_per_word_app/fronted/style/app_colors.dart';
+
+abstract class AppStyle {
+  static const fontStyle = TextStyle(
+    fontSize: 15,
+    color: AppColors.textColor,
+    fontFamily: 'Poppins',
+  );
+}
