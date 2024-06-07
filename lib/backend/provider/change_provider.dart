d600a28bbf3872331dcefd8d8cdbc5bc0019b73a
unknown <shaxabaca17@gmail.com>
2024-06-07 12:09:34 +0500

    abdulaziz

--- lib/backend/provider/change_provider.dart
+++ lib/backend/provider/change_provider.dart
@@ -1,12 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:ninenty_second_per_word_app/fronted/style/app_colors.dart';
 
 class CounterProvider extends ChangeNotifier {
   Color backcolor1 = AppColors.backgroundColor;
 
-  changeColor1() {
-    int a = 0;
-  }
+  changeColor1() {}
 }
